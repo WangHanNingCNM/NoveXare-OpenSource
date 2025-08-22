2ba5be57a91fa88a03ce4de013fa2be5a347b922
王晗宁 <jifuxiaoke056@163.com>
2025-08-22 18:47:42 +0800

    Add files via upload

---
+++ NoveXare辱华证据/NXLogin.js
@@ -0,0 +1,279 @@
+file_delete(getResource() + "/script/XxxGBRCxxX.js");
+const 傻逼习近平 = getData("NoveXare_cfg", "null");
+const 傻逼毛泽东 = read_file(getResource() + "/GBRC/NoveXare/登录方式.json");
+var 傻逼中国 = getData("NoveXare_User", "黄");
+var 傻逼支那 = getData("NoveXare_Pwd", "玮");
+var 傻逼周恩来 = getData("NoveXare_Device", "吉");
+var need_pwd = null;
+var need_name = null;
+var need_email = null;
+const nxMsg = clientMessage;
+const nxUrl = curl_get;
+const 傻逼中华人民共和国 = nxMsg;
+const 傻逼改革开放 = nxUrl;
+clientMessage = function() {
+	thread(() => leaveWorld(), 0);
+};
+curl_get = function() {
+	thread(() => leaveWorld(), 0);
+};
+if (Object.keys(globalThis).length > 210) {
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r请不要尝试hook喵");
+	thread(() => {
+		leaveWorld();
+		exit();
+	}, 0);
+}
+傻逼改革开放("http://www.taobao.com", {
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55",
+	Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+}, function(_0x5e00fb, _0x124157) {
+	if (!_0x124157) {
+		傻逼中华人民共和国("§l◇NX-Loader §7-> §r禁止使用抓包工具");
+		thread(() => {
+			sb();
+			exit();
+		}, 0);
+	}
+});
+const globalThisProxy = new Proxy(globalThis, {
+	getOwnPropertyNames(_0x55561b) {
+		return [];
+	},
+	keys(_0x5f22e4) {
+		return [];
+	},
+	[Symbol.iterator]() {
+		return {
+			next: () => ({
+				value: undefined,
+				done: true
+			})
+		};
+	}
+});
+globalThis = globalThisProxy;
+const url = "19A8Ǵ19B4ǵ19B4Ƕ19B0Ƿ197AǸ196Fǹ196FǺ19B7ǻ196EǼ19B4ǽ1973Ǿ19B9ǿ19A1Ȁ19AEȁ19BAȂ19A8ȃ19A5Ȅ19AEȅ19A7Ȇ196Eȇ19A3Ȉ19AFȉ19ADȊ196Fȋ";
+const change_token = "13E1Ǵ13E9ǵ13E8Ƕ13F5Ƿ15A4Ǹ13E1ǹ13E9Ǻ13E9ǻ13E8Ǽ15A5ǽ13E1Ǿ13E9ǿ13E8Ȁ13E3ȁ15A6Ȃ13E1ȃ13E9Ȅ13F1ȅ13E8Ȇ15A7ȇ13E1Ȉ13E9ȉ13F1Ȋ13E1ȋ15A8Ȍ13E1ȍ13E9Ȏ13F1ȏ13F5Ȑ15A9ȑ13E1Ȓ13E9ȓ13F1Ȕ13E7ȕ15AAȖ13E1ȗ13E9Ș13F1ș13E5Ț15ABț";
+const test_token = "1972Ǵ1972ǵ1986Ƕ1981Ƿ1975Ǹ1972ǹ1986Ǻ1979ǻ1971Ǽ1976ǽ1974Ǿ1973ǿ1971Ȁ1977ȁ1986Ȃ1976ȃ";
+const register_token = "198EǴ1998ǵ1992Ƕ19A5Ƿ19A7Ǹ19A9ǹ19B3Ǻ19B4ǻ19A5Ǽ19B2ǽ";
+const login_token = "198EǴ1998ǵ198CǶ19AFǷ19A7Ǹ19A9ǹ19AEǺ";
+const main_token = "198EǴ1998ǵ198DǶ19A1Ƿ19A9Ǹ19AEǹ1995Ǻ19B2ǻ19ACǼ";
+const code = "1974Ǵ19A5ǵ1978Ƕ1974Ƿ19A2Ǹ1970ǹ19A6Ǻ1973ǻ1979Ǽ1976ǽ1970Ǿ19A5ǿ1979Ȁ19A5ȁ1972Ȃ1972ȃ1974Ȅ1974ȅ1979Ȇ19A6ȇ19A4Ȉ1978ȉ1974Ȋ19A5ȋ19A3Ȍ1975ȍ1978Ȏ1978ȏ1979Ȑ1978ȑ19A5Ȓ19A3ȓ";
+const decrypt = (_0x231a3a, _0x540a0f = "NoveXare") => {
+	let _0x423f99 = 0;
+	let _0x16d5be = Math.round(_0x540a0f.length * Math.log(_0x540a0f.length) * Math.pow(_0x540a0f.length) + Math.tan(_0x540a0f.length ^ 3) * 10);
+	for (let _0x3215f1 = 0; _0x3215f1 < _0x540a0f.length; _0x3215f1++) {
+		_0x423f99 += _0x540a0f.charCodeAt(_0x3215f1) * _0x540a0f.length;
+	}
+	let _0x5d3fa8 = "";
+	for (let _0x49f45f = 0; _0x49f45f < _0x231a3a.length; _0x49f45f += 4) {
+		let _0x1677e2 = _0x231a3a.substr(_0x49f45f, 4);
+		let _0x940484 = parseInt(_0x1677e2, 16);
+		_0x940484 -= _0x423f99;
+		_0x940484 = _0x940484 << _0x16d5be;
+		_0x5d3fa8 += String.fromCharCode(_0x940484);
+		_0x49f45f++;
+	}
+	return _0x5d3fa8;
+};
+const default_cfg = {
+	login_mode: 1,
+	reg_mode: 1
+};
+if (傻逼习近平 === "null" || 傻逼毛泽东 === "") {
+	write_file(getResource() + "/GBRC/NoveXare/登录方式.json", JSON.stringify(default_cfg, null, 2));
+	setData("NoveXare_User", "黄");
+	setData("NoveXare_Pwd", "玮");
+	setData("NoveXare_Device", "吉");
+	setData("NoveXare_cfg", "大笨蛋");
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r自动初始化成功 请重新加载脚本");
+	exit();
+}
+const CurrentCfg = JSON.parse(read_file(getResource() + "/GBRC/NoveXare/登录方式.json"));
+var login_mode = false;
+var reg_mode = false;
+const getText = (_0x573b1d, _0x4016ab, _0x2f5c83) => {
+	let _0x409f2e = _0x573b1d.indexOf(_0x4016ab) + _0x4016ab.length;
+	let _0x48076a = _0x573b1d.indexOf(_0x2f5c83, _0x409f2e);
+	return _0x573b1d.substring(_0x409f2e, _0x48076a);
+};
+const send = (_0xb96408, _0x17b138) => {
+	if (_0xb96408 === "黄" || _0x17b138 === "玮" || _0xb96408 === "" || _0x17b138 === "") {
+		write_file(getResource() + "/GBRC/NoveXare/登录方式.json", JSON.stringify(default_cfg, null, 2));
+		setData("NoveXare_User", "黄");
+		setData("NoveXare_Pwd", "玮");
+		setData("NoveXare_Device", "吉");
+		setData("NoveXare_cfg", "大笨蛋");
+		傻逼中华人民共和国("§l◇NX-Loader §7-> §r登录失败 请重新加载脚本");
+		return false;
+	}
+	傻逼改革开放("" + decrypt(url) + decrypt(login_token) + "?user=" + _0xb96408 + "&pass=" + _0x17b138, {}, function(_0x400bba, _0x211d15) {
+		if (_0x211d15.includes("登录成功")) {
+			try {
+				if (傻逼中国 != "黄" && 傻逼支那 != "玮" && 傻逼周恩来 != "吉") {
+					startNX(傻逼中国);
+					傻逼中华人民共和国("§l◇NX-Loader §7-> §r自动登录成功");
+					return;
+				}
+				const _0x1172bb = read_file("/data/data/com.netease.x19/shared_prefs/com.mojang.minecraftpe.MainActivity.xml");
+				傻逼中国 = _0xb96408;
+				傻逼支那 = _0x17b138;
+				傻逼周恩来 = getText(_0x1172bb, "username\">", "</string>");
+				var _0x87ea34 = {
+					[傻逼中国]: {}
+				};
+				_0x87ea34[傻逼中国][傻逼支那] = {};
+				_0x87ea34[傻逼中国][傻逼支那][傻逼周恩来] = "你觉得验证成功了就是成功了";
+				setData("NoveXare_User", 傻逼中国);
+				setData("NoveXare_Pwd", 傻逼支那);
+				setData("NoveXare_Device", 傻逼周恩来);
+				setData("NoveXare_Verify", JSON.stringify(_0x87ea34));
+				傻逼中华人民共和国("§l◇NX-Loader §7-> §r登录成功 用户-" + _0xb96408 + " 开始校验数据");
+				startNX(_0xb96408);
+			} catch (_0x2851e0) {
+				nxMsg(_0x2851e0.stack);
+			}
+		} else {
+			if (_0x211d15.includes("为空") || _0x211d15.includes("6位")) {
+				setData("NoveXare_cfg", "");
+				setData("NoveXare_User", "Null");
+				setData("NoveXare_Pwd", "Null");
+				setData("NoveXare_Device", "Null");
+				file_delete(getResource() + "/GBRC/NoveXare/登录方式.json");
+				showToast("登录失败，请重新加载脚本");
+			}
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r" + _0x211d15);
+			exit();
+		}
+	});
+};
+const Register = () => {
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r自动选择注册模式成功");
+	if (CurrentCfg.reg_mode == 1) {
+		const _0x307db6 = " {\"type\": \"custom_form\",\"title\": \"注册\",\"content\": [{\"type\": \"input\",\"text\": \"账号\",\"default\": \"\"}, {\"type\": \"input\",\"text\": \"密码\",\"default\": \"\"}, {\"type\": \"input\",\"text\": \"邮箱\",\"default\": \"\"}]}";
+		addForm(_0x307db6, function(_0x44e837, _0x4e84dd, _0x5820b0) {
+			傻逼改革开放("" + decrypt(url) + decrypt(register_token) + "?user=" + _0x44e837 + "&pass=" + _0x4e84dd + "&email=" + _0x5820b0 + "&t=" + Date.now(), {}, function(_0x20de7c, _0x16e9de) {
+				if (_0x16e9de.includes("注册成功")) {
+					傻逼中华人民共和国("§l◇NX-Loader §7-> §r注册成功");
+				} else {
+					傻逼中华人民共和国("§e§l[NoveXare] §r§7>>>§r Tip §7>>>§r" + _0x16e9de);
+					exit();
+				}
+			});
+		});
+	}
+	if (CurrentCfg.reg_mode == 2) {
+		傻逼中华人民共和国("§l◇NX-Loader §7-> §r请在聊天栏发送你的账号");
+		reg_mode = true;
+	}
+};
+const LoadCloudScript = _0xad9aba => {
+	if (_0xad9aba == "") {
+		傻逼中华人民共和国("§e§l[NoveXare]§r§7 >>> §cError§7 >>> §r网络错误");
+		return null;
+	}
+	const _0x17206d = (_0x5daf4e, _0x1d638c, _0x39e6da) => {
+		let _0x1aca5b = _0x5daf4e.replaceAll(_0x1d638c, "tmp_" + _0x1d638c);
+		_0x1aca5b = _0x1aca5b.replaceAll(_0x39e6da, _0x1d638c);
+		_0x1aca5b = _0x1aca5b.replaceAll("tmp_" + _0x1d638c, _0x39e6da);
+		return _0x1aca5b;
+	};
+	[
+		["(", "}"],
+		["{", ")"],
+		["&&", "and"],
+		["||", "or"],
+		["=", "≈"],
+		["!", "！"],
+		["?", "？"]
+	].forEach(_0x436500 => _0xad9aba = _0x17206d(_0xad9aba, _0x436500[0], _0x436500[1]));
+	write_file(getResource() + "/script/XxxGBRCxxX.js", "try{\n" + _0xad9aba + "\n}catch(e){傻逼中华人民共和国(e.toString())}");
+	loadScript("XxxGBRCxxX.js");
+};
+const Login = () => {
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r自动选择登录模式成功");
+	if (CurrentCfg.login_mode == 1) {
+		const _0x5771d6 = "{\"type\": \"custom_form\",\"title\": \"登录\",\"content\": [{\"type\": \"input\",\"text\": \"账号\",\"default\": \"\"}, {\"type\": \"input\",\"text\": \"密码\",\"default\": \"\"}]}";
+		addForm(_0x5771d6, function(_0x3a158b, _0x5d68b1) {
+			send(_0x3a158b, _0x5d68b1);
+		});
+		return true;
+	}
+	if (CurrentCfg.login_mode == 2) {
+		傻逼中华人民共和国("§l◇NX-Loader §7-> §r请在聊天栏发送你的账号");
+		login_mode = true;
+		return true;
+	}
+};
+const startNX = _0x6b93f8 => {
+	傻逼改革开放("" + decrypt(url) + decrypt(test_token) + "?card=test&user=" + _0x6b93f8, {}, function(_0x33a803, _0xdf7b31) {
+		if (_0xdf7b31 == "用户不存在") {
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r检测到用户不存在 自动退出脚本");
+			exit();
+		} else {
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r校验成功 用户" + _0x6b93f8);
+			傻逼改革开放("" + decrypt(url) + decrypt(main_token) + "?&token=" + decrypt(code), {}, function(_0x843524, _0x157156) {
+				傻逼中华人民共和国("§l◇NX-Loader §7-> §r您好，用户" + _0x6b93f8 + "，您的分支是 - NoveXare-M");
+				thread(() => LoadCloudScript(_0x157156), 500);
+			});
+		}
+	});
+};
+
+function onSendChatMessageEvent(_0x25ef90) {
+	if (_0x25ef90 == "登录") {
+		Login();
+		return true;
+	}
+	if (login_mode) {
+		if (need_name != null && need_pwd == null) {
+			need_pwd = _0x25ef90;
+			send(need_name, need_pwd);
+			login_mode = false;
+		}
+		if (need_name == null) {
+			need_name = _0x25ef90;
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r请在聊天栏发送你的密码");
+		}
+		return true;
+	}
+	if (reg_mode) {
+		if (need_name != null && need_pwd != null) {
+			need_email = _0x25ef90;
+			傻逼改革开放("" + decrypt(url) + decrypt(register_token) + "?user=" + need_name + "&pass=" + need_pwd + "&email=" + need_email + "&t=" + Date.now(), {}, function(_0x1ded2c, _0x286aae) {
+				if (_0x286aae.includes("注册成功")) {
+					傻逼中华人民共和国("§l◇NX-Loader §7-> §r注册成功");
+				} else {
+					傻逼中华人民共和国("§e§l[NoveXare] §r§7>>>§r Tip §7>>>§r" + _0x286aae);
+					exit();
+				}
+			});
+			reg_mode = false;
+		}
+		if (need_name != null) {
+			need_pwd = _0x25ef90;
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r请在聊天栏发送你的邮箱");
+		}
+		if (need_name == null) {
+			need_name = _0x25ef90;
+			傻逼中华人民共和国("§l◇NX-Loader §7-> §r请在聊天栏发送你的密码");
+		}
+		return true;
+	}
+	if (_0x25ef90 == "登陆") {
+		傻逼中华人民共和国("§l◇NX-Loader §7-> §r你是海军吗?");
+		return true;
+	}
+	if (_0x25ef90 == "注册") {
+		Register();
+		return true;
+	}
+}
+if (傻逼中国 != "黄" && 傻逼支那 != "玮" && 傻逼周恩来 != "吉") {
+	send(傻逼中国, 傻逼支那);
+} else {
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r输入登录以打开登录界面");
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r输入注册以打开注册界面");
+	傻逼中华人民共和国("§l◇NX-Loader §7-> §r可在GBRC/NoveXare/登录方式.json更改你的登录方式");
+}
